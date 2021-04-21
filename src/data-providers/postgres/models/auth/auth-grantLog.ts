c6bf93ce728ec5bcf30dec95a38d9a16549b392a
Sam Lanning <sam@samlanning.com>
2021-04-21 12:00:36 +0100

    🎨 Use a common closure function for all models
    Given that all models currently share the same base database operations,
    rather than redefining them for every model, we can define them once and
    reuse them across all models.

--- src/data-providers/postgres/models/auth/auth-grantLog.ts
+++ src/data-providers/postgres/models/auth/auth-grantLog.ts
@@ -1,6 +1,6 @@
-import { ModelBase, TableBase } from '../common/base';
+import { createModel, TableBaseWithTimeStamps } from '../common/base';
 
-export interface AuthGrantLogTable extends TableBase {
+export interface AuthGrantLogTable extends TableBaseWithTimeStamps {
   actor: number;
   date: Date;
   grantee: number;
@@ -8,22 +8,4 @@ export interface AuthGrantLogTable extends TableBase {
   target: number;
 }
 
-type AuthGrantLogModel = ModelBase<AuthGrantLogTable>;
-
-export const authGrantLogModel: AuthGrantLogModel = {
-  tableName: 'authGrantLog',
-  table: (db) => db(authGrantLogModel.tableName),
-  getOne: (db, id) => db(authGrantLogModel.tableName).select({ where: { id } }),
-  getAll: (db) => db(authGrantLogModel.tableName).select('*'),
-  create: (db, authGrantLog) =>
-    db(authGrantLogModel.tableName).insert(authGrantLog),
-  update: async (db, id, changes) => {
-    await db(authGrantLogModel.tableName).update(changes).where({ id });
-    return authGrantLogModel.getOne(db, id);
-  },
-  deleteOne: async (db, id) => {
-    await db(authGrantLogModel.tableName)
-      .update({ deletedAt: new Date() })
-      .where({ id });
-  },
-};
+export const authGrantLogModel = createModel<AuthGrantLogTable>('authGrantLog');
--- src/data-providers/postgres/models/auth/auth-target.ts
+++ src/data-providers/postgres/models/auth/auth-target.ts
@@ -1,25 +1,8 @@
-import { ModelBase, TableBase } from '../common/base';
+import { createModel, TableBaseWithTimeStamps } from '../common/base';
 
-export interface AuthTargetTable extends TableBase {
+export interface AuthTargetTable extends TableBaseWithTimeStamps {
   targetId: number;
   type: string;
 }
 
-type AuthTargetModel = ModelBase<AuthTargetTable>;
-
-export const authTargetModel: AuthTargetModel = {
-  tableName: 'authTarget',
-  table: (db) => db(authTargetModel.tableName),
-  getOne: (db, id) => db(authTargetModel.tableName).select({ where: { id } }),
-  getAll: (db) => db(authTargetModel.tableName).select('*'),
-  create: (db, authTarget) => db(authTargetModel.tableName).insert(authTarget),
-  update: async (db, id, changes) => {
-    await db(authTargetModel.tableName).update(changes).where({ id });
-    return authTargetModel.getOne(db, id);
-  },
-  deleteOne: async (db, id) => {
-    await db(authTargetModel.tableName)
-      .update({ deletedAt: new Date() })
-      .where({ id });
-  },
-};
+export const authTargetModel = createModel<AuthTargetTable>('authTarget');
