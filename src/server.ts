58b0bd99e8c31a1255476dd32d32127f932dddf7
Ash <aisha@astroash.com>
2021-04-21 11:33:38 +0100

    🚨 linting woes

--- src/server.ts
+++ src/server.ts
@@ -39,10 +39,9 @@ async function StartServer() {
     },
   });
 
-
   await apolloServer.applyMiddleware({
     app: server,
-    path: '/v4/graphql'
+    path: '/v4/graphql',
   });
 
   await apolloServer.installSubscriptionHandlers(server.listener);
