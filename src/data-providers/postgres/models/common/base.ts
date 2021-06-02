7a87a1680feb2230284088fa34c64a4dd2473356
Pl217 <pl_217@live.com>
2021-06-02 16:43:21 +0200

    :lipstick: Format `src/data-providers/postgres/models/common/base.ts`

--- src/data-providers/postgres/models/common/base.ts
+++ src/data-providers/postgres/models/common/base.ts
@@ -8,23 +8,23 @@ export interface TableBaseWithTimeStamps extends TableBase {
   updatedAt: Date;
 }
 
-export const createModel = <T extends TableBaseWithTimeStamps>(
-  tableName: string
-) => (db: Knex) => {
-  const table: Knex.QueryBuilder<any, T> = db(tableName);
-  const model = {
-    tableName,
-    table: () => table,
-    getOne: (id: number) => table.select({ where: { id } }) as Promise<T>,
-    getAll: () => table.select('*') as Promise<T[]>,
-    create: (data: T) => table.insert(data),
-    update: async (id: number, changes: Partial<T>) => {
-      await table.update(changes).where({ id });
-      return model.getOne(id);
-    },
-    deleteOne: async (id: number) => {
-      await table.update({ deletedAt: new Date() }).where({ id });
-    },
+export const createModel =
+  <T extends TableBaseWithTimeStamps>(tableName: string) =>
+  (db: Knex) => {
+    const table: Knex.QueryBuilder<any, T> = db(tableName);
+    const model = {
+      tableName,
+      table: () => table,
+      getOne: (id: number) => table.select({ where: { id } }) as Promise<T>,
+      getAll: () => table.select('*') as Promise<T[]>,
+      create: (data: T) => table.insert(data),
+      update: async (id: number, changes: Partial<T>) => {
+        await table.update(changes).where({ id });
+        return model.getOne(id);
+      },
+      deleteOne: async (id: number) => {
+        await table.update({ deletedAt: new Date() }).where({ id });
+      },
+    };
+    return model;
   };
-  return model;
-};
