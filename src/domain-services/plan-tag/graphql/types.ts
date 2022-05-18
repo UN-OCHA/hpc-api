3fae92926f1815c01f78f7a5e405d0594ce5f650
krishnareddy.tadi <krishnareddy.tadi@trigyn.com>
2022-05-18 20:36:48 +0530

    Create plantag related resolvers, types and service files

---
+++ src/domain-services/plan-tag/graphql/types.ts
@@ -0,0 +1,87 @@
+import { Brand } from '@unocha/hpc-api-core/src/util/types';
+import { MaxLength } from 'class-validator';
+import {
+  Field,
+  ID,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from 'type-graphql';
+
+export enum RevisionState {
+  projectsOnly = 'projectsOnly',
+  none = 'none',
+  planDataAndProjects = 'planDataAndProjects',
+  planDataOnly = 'planDataOnly',
+}
+registerEnumType(RevisionState, {
+  name: 'RevisionState',
+});
+
+export enum PlanTagType {
+  major = 'major',
+  minor = 'minor',
+  custom = 'custom',
+}
+registerEnumType(PlanTagType, {
+  name: 'Type',
+});
+
+@ObjectType()
+export default class PlanTag {
+  @Field(() => ID)
+  id: Brand<number, { readonly s: unique symbol }, 'planTag.id'>;
+
+  @Field()
+  planId: number;
+
+  @Field()
+  @MaxLength(255)
+  name: string;
+
+  @Field({ nullable: true })
+  @MaxLength(50)
+  type: PlanTagType;
+
+  @Field()
+  public: boolean;
+
+  @Field({ nullable: true })
+  @MaxLength(255)
+  comments: string;
+
+  @Field({ nullable: true })
+  @MaxLength(255)
+  revisionState: RevisionState;
+}
+
+@InputType({ description: 'New plan tag data' })
+export class AddPlanTagInput implements Partial<PlanTag> {
+  @Field(() => ID)
+  planId: Brand<number, { readonly s: unique symbol }, 'Plan ID'>;
+
+  @Field()
+  @MaxLength(50)
+  type: PlanTagType;
+
+  @Field()
+  major: boolean;
+
+  @Field({ nullable: true })
+  @MaxLength(255)
+  comments: string;
+
+  @Field({ nullable: true })
+  @MaxLength(255)
+  revisionState: RevisionState;
+
+  @Field(() => [ID], { nullable: true })
+  reportingPeriodIds: Brand<
+    number,
+    { readonly s: unique symbol },
+    'planReportingPeriod ID'
+  >[];
+
+  @Field()
+  publishMeasurements: boolean;
+}
