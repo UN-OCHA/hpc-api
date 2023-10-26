7af7f140da5e22d5a83c5e8a2b3e02487deefd09
manelcecs <manuelc.p.g@gmail.com>
2023-10-26 08:47:48 +0200

    Add utils for tests

---
+++ tests/utils/connection.ts
@@ -0,0 +1,33 @@
+import * as t from 'io-ts';
+import Knex from 'knex';
+
+const CONFIG = t.type({
+  host: t.string,
+  port: t.number,
+  user: t.string,
+  database: t.string,
+});
+
+/**
+ * Initialize a new Postgres provider
+ */
+export async function createDbConnection(connection: t.TypeOf<typeof CONFIG>) {
+  const knex = Knex({
+    client: 'pg',
+    connection,
+    pool: { min: 0, max: 10, idleTimeoutMillis: 500 },
+    debug: false,
+  });
+
+  // Verify the connection before proceeding
+  try {
+    await knex.raw('SELECT now()');
+
+    return knex;
+  } catch (error) {
+    console.log(error);
+    throw new Error(
+      'Unable to connect to Postgres via Knex. Ensure a valid connection.'
+    );
+  }
+}
