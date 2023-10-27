03f81d29d0ce810ddfedf5c69b8fb26b0c0584d0
manelcecs <manuelc.p.g@gmail.com>
2023-10-27 12:22:57 +0200

    Refactor move methods to individual services

--- src/domain-services/location/location-service.ts
+++ src/domain-services/location/location-service.ts
@@ -2,6 +2,8 @@ import { type Database } from '@unocha/hpc-api-core/src/db/type';
 import { type InstanceDataOfModel } from '@unocha/hpc-api-core/src/db/util/raw-model';
 import { createBrandedValue } from '@unocha/hpc-api-core/src/util/types';
 import { Service } from 'typedi';
+import { FlowLocation } from '../flows/graphql/types';
+import { Op } from '@unocha/hpc-api-core/src/db/util/conditions';
 
 @Service()
 export class LocationService {
@@ -25,5 +27,23 @@ export class LocationService {
     return await models.location.find({
       where: { name: { [models.Op.ILIKE]: `%${name}%` } },
     });
+  }
+
+  async getFlowObjectLocations(
+    locationsFO: any[],
+    models: Database
+  ): Promise<FlowLocation[]> {
+    const locations = await models.location.find({
+      where: {
+        id: {
+          [Op.IN]: locationsFO.map((locFO) => locFO.objectID),
+        },
+      },
+    });
+
+    return locations.map((loc) => ({
+      id: loc.id.valueOf(),
+      name: loc.name!,
+    }));
   }
 }
---
+++ src/domain-services/organizations/organization-service.ts
@@ -0,0 +1,24 @@
+import { Database } from "@unocha/hpc-api-core/src/db";
+import { Service } from "typedi";
+import { Op } from '@unocha/hpc-api-core/src/db/util/conditions';
+
+@Service()
+export class OrganizationService {
+
+    async getFlowObjectOrganizations(organizationsFO: any[], models: Database){
+        const organizations = await models.organization.find({
+            where: {
+              id: {
+                [Op.IN]: organizationsFO.map((orgFO) => orgFO.objectID),
+              },
+            },
+          });
+      
+          return organizations.map((org) => ({
+            id: org.id,
+            refDirection: organizationsFO.find((orgFO) => orgFO.objectID === org.id)
+              .refDirection,
+            name: org.name,
+          }));
+    }
+}
