bcd7cf1ad913ab931cb5018292ac913bbc6db50d
manelcecs <manuelc.p.g@gmail.com>
2023-10-30 10:40:08 +0100

    Add Pagination utils with tests

---
+++ tests/unit/pagination.spec.ts
@@ -0,0 +1,72 @@
+import { Op } from '@unocha/hpc-api-core/src/db/util/conditions';
+import { prepareConditionFromCursor } from '../../src/utils/graphql/pagination';
+
+describe('Based on cursor and order for pagination', () => {
+  describe('Order is asc', () => {
+    const sortCondition = { column: 'id', order: 'asc' as const };
+
+    it("Should return 'GT' when afterCursor is defined", () => {
+      const afterCursor = 1;
+      const beforeCursor = undefined;
+      const result = prepareConditionFromCursor(
+        sortCondition,
+        afterCursor,
+        beforeCursor
+      );
+      expect(result.id).toEqual({ [Op.GT]: afterCursor });
+    });
+
+    it("Should return 'LT' when beforeCursor is defined", () => {
+      const afterCursor = undefined;
+      const beforeCursor = 1;
+      const result = prepareConditionFromCursor(
+        sortCondition,
+        afterCursor,
+        beforeCursor
+      );
+      expect(result.id).toEqual({ [Op.LT]: beforeCursor });
+    });
+
+    it('Should throw an error when both afterCursor and beforeCursor are defined', () => {
+      const afterCursor = 1;
+      const beforeCursor = 2;
+      expect(() =>
+        prepareConditionFromCursor(sortCondition, afterCursor, beforeCursor)
+      ).toThrowError('Cannot use before and after cursor at the same time');
+    });
+  });
+
+  describe("Order is 'desc'", () => {
+    const sortCondition = { column: 'id', order: 'desc' as const };
+
+    it("Should return 'LT' when afterCursor is defined", () => {
+      const afterCursor = 1;
+      const beforeCursor = undefined;
+      const result = prepareConditionFromCursor(
+        sortCondition,
+        afterCursor,
+        beforeCursor
+      );
+      expect(result.id).toEqual({ [Op.LT]: afterCursor });
+    });
+
+    it("Should return 'GT' when beforeCursor is defined", () => {
+      const afterCursor = undefined;
+      const beforeCursor = 1;
+      const result = prepareConditionFromCursor(
+        sortCondition,
+        afterCursor,
+        beforeCursor
+      );
+      expect(result.id).toEqual({ [Op.GT]: beforeCursor });
+    });
+
+    it('Should throw an error when both afterCursor and beforeCursor are defined', () => {
+      const afterCursor = 1;
+      const beforeCursor = 2;
+      expect(() =>
+        prepareConditionFromCursor(sortCondition, afterCursor, beforeCursor)
+      ).toThrowError('Cannot use before and after cursor at the same time');
+    });
+  });
+});
