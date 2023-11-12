539674bf9ab3642616351d3a0c7f562727555894
manelcecs <manuelc.p.g@gmail.com>
2023-11-12 14:20:37 +0100

    Add FlowObject Strategy.

---
+++ src/domain-services/flows/strategy/impl/flow-object-conditions-strategy.ts
@@ -0,0 +1,98 @@
+import { Database } from '@unocha/hpc-api-core/src/db';
+import { Cond, Op } from '@unocha/hpc-api-core/src/db/util/conditions';
+import { Service } from 'typedi';
+import { FlowObjectService } from '../../../flow-object/flow-object-service';
+import { FlowService } from '../../flow-service';
+import {
+  FlowSearchStrategy,
+  FlowSearchStrategyResponse,
+} from '../flow-search-strategy';
+
+@Service()
+export class FlowObjectFiltersStrategy implements FlowSearchStrategy {
+  constructor(
+    private readonly flowService: FlowService,
+    private readonly flowObjectService: FlowObjectService
+  ) {}
+
+  async search(
+    flowConditions: Map<string, any>,
+    orderBy: any,
+    limit: number,
+    cursorCondition: any,
+    models: Database
+  ): Promise<FlowSearchStrategyResponse> {
+    const flowObjectsConditions: Map<
+      string,
+      Map<string, number[]>
+    > = flowConditions.get('flowObjects');
+    const flowEntityConditions = flowConditions.get('flow');
+
+    const flowObjectWhere = this.mapFlowObjectConditionsToWhereClause(
+      flowObjectsConditions
+    );
+
+    // Obtain flowIDs based on provided flowObject conditions
+    const flowIDsFromFilteredFlowObjects =
+      await this.flowObjectService.getFlowIdsFromFlowObjects(
+        models,
+        flowObjectWhere
+      );
+
+    // Combine conditions from flowObjects FlowIDs and flow conditions
+    const mergedFlowConditions = {
+      ...flowEntityConditions,
+      flowID: {
+        [Op.IN]: flowIDsFromFilteredFlowObjects,
+      },
+    };
+
+    const conditions = { ...cursorCondition, ...mergedFlowConditions };
+
+    // Obtain flows and flowCount based on flowIDs from filtered flowObjects
+    // and flow conditions
+    const [flows, countRes] = await Promise.all([
+      this.flowService.getFlows(models, conditions, orderBy, limit),
+      this.flowService.getFlowsCount(models, conditions),
+    ]);
+
+    // Map count result query to count object
+    const countObject = countRes[0] as { count: number };
+
+    return { flows, count: countObject.count };
+  }
+
+  /*
+   * Map structure:
+   * {
+   *   KEY = objectType: string,
+   *   VALUE = {
+   *         KEY = refDirection: string,
+   *         VALUE = [objectID: number]
+   *         }
+   * }
+   */
+  private mapFlowObjectConditionsToWhereClause(
+    flowObjectConditions: Map<string, Map<string, number[]>>
+  ): any {
+    let flowObjectWhere: any = {};
+    for (const [objectType, refDirectionMap] of flowObjectConditions) {
+      for (const [refDirection, objectIDs] of refDirectionMap) {
+        flowObjectWhere = {
+          ...flowObjectWhere,
+            objectID: {
+              [Op.IN]: objectIDs,
+            },
+            refDirection: {
+              [Op.IN]: refDirection,
+            },
+            objectType: {
+              [Op.IN]: objectType,
+            },
+        };
+      }
+    }
+
+    return flowObjectWhere;
+  }
+}
--- src/domain-services/flows/strategy/impl/only-flow-conditions-strategy.ts
+++ src/domain-services/flows/strategy/impl/only-flow-conditions-strategy.ts
@@ -1,250 +1,35 @@
 import { Database } from '@unocha/hpc-api-core/src/db';
 import { Service } from 'typedi';
 import { FlowId } from '@unocha/hpc-api-core/src/db/models/flow';
-import { CategoryService } from '../../../categories/category-service';
-import { ExternalReferenceService } from '../../../external-reference/external-reference-service';
-import { LocationService } from '../../../location/location-service';
-import { OrganizationService } from '../../../organizations/organization-service';
-import { PlanService } from '../../../plans/plan-service';
-import { ReportDetailService } from '../../../report-details/report-detail-service';
-import { UsageYearService } from '../../../usage-years/usage-year-service';
-import { FlowLinkService } from '../../flow-link-service';
 import { FlowService } from '../../flow-service';
-import { FlowParkedParentSource, FlowSearchResult } from '../../graphql/types';
-import { FlowSearchStrategy } from '../flow-search-strategy';
+import {
+  FlowSearchStrategy,
+  FlowSearchStrategyResponse,
+} from '../flow-search-strategy';
 import { Op } from '@unocha/hpc-api-core/src/db/util/conditions';
 
 @Service()
 export class OnlyFlowFiltersStrategy implements FlowSearchStrategy {
-  constructor(
-    private readonly organizationService: OrganizationService,
-    private readonly locationService: LocationService,
-    private readonly planService: PlanService,
-    private readonly usageYearService: UsageYearService,
-    private readonly categoryService: CategoryService,
-    private readonly flowLinkService: FlowLinkService,
-    private readonly externalReferenceService: ExternalReferenceService,
-    private readonly reportDetailService: ReportDetailService,
-    private readonly flowService: FlowService
-  ) {}
+  constructor(private readonly flowService: FlowService) {}
 
   async search(
-    flowConditions: Map<string, any>,
+    flowConditions: any,
     orderBy: any,
     limit: number,
     cursorCondition: any,
     models: Database
-  ): Promise<FlowSearchResult> {
-    // Fetch one more item to check for hasNextPage
-    const limitComputed = limit + 1;
-
+  ): Promise<FlowSearchStrategyResponse> {
     // Build conditions object
     const conditions: any = { ...cursorCondition, ...flowConditions };
 
-    console.log('conditions in OnlyFlowFiltersStrategy', conditions);
     const [flows, countRes] = await Promise.all([
-      this.flowService.getFlows(models, conditions, orderBy, limitComputed),
+      this.flowService.getFlows(models, conditions, orderBy, limit),
       this.flowService.getFlowsCount(models, conditions),
     ]);
 
-    const hasNextPage = flows.length > limit;
-    if (hasNextPage) {
-      flows.pop(); // Remove the extra item used to check hasNextPage
-    }
+    // Map count result query to count object
+    const countObject = countRes[0] as { count: number };
 
-    const count = countRes[0] as { count: number };
-
-    const flowIds: FlowId[] = flows.map((flow) => flow.id);
-
-    const organizationsFO: any[] = [];
-    const locationsFO: any[] = [];
-    const plansFO: any[] = [];
-    const usageYearsFO: any[] = [];
-
-    const [externalReferencesMap] = await Promise.all([
-      this.externalReferenceService.getExternalReferencesForFlows(
-        flowIds,
-        models
-      ),
-      this.getFlowObjects(
-        flowIds,
-        models,
-        organizationsFO,
-        locationsFO,
-        plansFO,
-        usageYearsFO
-      ),
-    ]);
-
-    const flowLinksMap = await this.flowLinkService.getFlowLinksForFlows(
-      flowIds,
-      models
-    );
-
-    const [
-      categoriesMap,
-      organizationsMap,
-      locationsMap,
-      plansMap,
-      usageYearsMap,
-      reportDetailsMap,
-    ] = await Promise.all([
-      this.categoryService.getCategoriesForFlows(flowLinksMap, models),
-      this.organizationService.getOrganizationsForFlows(
-        organizationsFO,
-        models
-      ),
-      this.locationService.getLocationsForFlows(locationsFO, models),
-      this.planService.getPlansForFlows(plansFO, models),
-      this.usageYearService.getUsageYearsForFlows(usageYearsFO, models),
-      this.reportDetailService.getReportDetailsForFlows(flowIds, models),
-    ]);
-
-    const items = await Promise.all(
-      flows.map(async (flow) => {
-        const flowLink = flowLinksMap.get(flow.id) || [];
-        const categories = categoriesMap.get(flow.id) || [];
-        const organizations = organizationsMap.get(flow.id) || [];
-        const locations = [...(locationsMap.get(flow.id) || [])];
-        const plans = plansMap.get(flow.id) || [];
-        const usageYears = usageYearsMap.get(flow.id) || [];
-        const externalReferences = externalReferencesMap.get(flow.id) || [];
-        const reportDetails = reportDetailsMap.get(flow.id) || [];
-
-        let parkedParentSource: FlowParkedParentSource[] = [];
-        if (flow.activeStatus && flowLink.length > 0) {
-          parkedParentSource = await this.getParketParents(
-            flow,
-            flowLink,
-            models
-          );
-        }
-
-        const childIDs: number[] = flowLinksMap
-          .get(flow.id)
-          ?.filter(
-            (flowLink) => flowLink.parentID === flow.id && flowLink.depth > 0
-          )
-          .map((flowLink) => flowLink.childID.valueOf()) as number[];
-
-        const parentIDs: number[] = flowLinksMap
-          .get(flow.id)
-          ?.filter(
-            (flowLink) => flowLink.childID === flow.id && flowLink.depth > 0
-          )
-          .map((flowLink) => flowLink.parentID.valueOf()) as number[];
-
-        return {
-          // Mandatory fields
-          id: flow.id.valueOf(),
-          versionID: flow.versionID,
-          amountUSD: flow.amountUSD.toString(),
-          createdAt: flow.createdAt.toISOString(),
-          updatedAt: flow.updatedAt.toISOString(),
-          activeStatus: flow.activeStatus,
-          restricted: flow.restricted,
-          // Optional fields
-          categories,
-          organizations,
-          locations,
-          plans,
-          usageYears,
-          childIDs,
-          parentIDs,
-          origAmount: flow.origAmount ? flow.origAmount.toString() : '',
-          origCurrency: flow.origCurrency ? flow.origCurrency.toString() : '',
-          externalReferences,
-          reportDetails,
-          parkedParentSource:
-            parkedParentSource.length > 0 ? parkedParentSource : null,
-          // Paged item field
-          cursor: flow.id.valueOf(),
-        };
-      })
-    );
-
-    return {
-      flows: items,
-      hasNextPage: limit <= flows.length,
-      hasPreviousPage: false, // TODO: cursorCondition['id'].GT !== undefined,
-      startCursor: flows.length ? flows[0].id.valueOf() : 0,
-      endCursor: flows.length ? flows[flows.length - 1].id.valueOf() : 0,
-      pageSize: flows.length,
-      sortField: orderBy.column,
-      sortOrder: orderBy.order,
-      total: count.count,
-    };
-  }
-
-  private async getFlowObjects(
-    flowIds: FlowId[],
-    models: Database,
-    organizationsFO: any[],
-    locationsFO: any[],
-    plansFO: any[],
-    usageYearsFO: any[]
-  ): Promise<void> {
-    const flowObjects = await models.flowObject.find({
-      where: {
-        flowID: {
-          [Op.IN]: flowIds,
-        },
-      },
-    });
-
-    flowObjects.forEach((flowObject) => {
-      if (flowObject.objectType === 'organization') {
-        organizationsFO.push(flowObject);
-      } else if (flowObject.objectType === 'location') {
-        locationsFO.push(flowObject);
-      } else if (flowObject.objectType === 'plan') {
-        plansFO.push(flowObject);
-      } else if (flowObject.objectType === 'usageYear') {
-        usageYearsFO.push(flowObject);
-      }
-    });
-  }
-
-  private async getParketParents(
-    flow: any,
-    flowLink: any[],
-    models: Database
-  ): Promise<any> {
-    const flowLinksDepth0 = flowLink.filter((flowLink) => flowLink.depth === 0);
-
-    const flowLinksParent = flowLinksDepth0.filter(
-      (flowLink) => flowLink.parentID === flow.id
-    );
-
-    const categories = await models.category.find({
-      where: {
-        group: 'flowType',
-        name: 'parked',
-      },
-    });
-
-    const categoriesIDs = categories.map((category) => category.id);
-
-    const categoryRef = await models.categoryRef.find({
-      where: {
-        categoryID: {
-          [Op.IN]: categoriesIDs,
-        },
-        versionID: flow.versionID,
-      },
-    });
-
-    const parentFlows = flowLinksParent
-      .filter((flowLink) => {
-        return categoryRef.some(
-          (categoryRef) =>
-            categoryRef.objectID.valueOf() === flowLink.parentID.valueOf()
-        );
-      })
-      .map((flowLink) => {
-        return flowLink.parentID.valueOf();
-      });
-
-    return parentFlows;
+    return { flows, count: countObject.count };
   }
 }
