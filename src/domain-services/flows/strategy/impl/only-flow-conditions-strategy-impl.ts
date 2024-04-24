fac809938fdeb4384f364dd339126a8a05bb94be
manelcecs <manuelc.p.g@gmail.com>
2024-04-24 12:31:55 +0200

    Add 'only-flow-conditions-strategy' implementation
    Add also utils to operate over flows

---
+++ src/domain-services/flows/strategy/impl/only-flow-conditions-strategy-impl.ts
@@ -0,0 +1,67 @@
+import { Cond, Op } from '@unocha/hpc-api-core/src/db/util/conditions';
+import { Service } from 'typedi';
+import { FlowService } from '../../flow-service';
+import { type FlowWhere } from '../../model';
+import {
+  type FlowSearchArgs,
+  type FlowSearchStrategy,
+  type FlowSearchStrategyResponse,
+} from '../flow-search-strategy';
+import {
+  mapFlowOrderBy,
+  prepareFlowConditions,
+  prepareFlowStatusConditions,
+} from './utils';
+
+@Service()
+export class OnlyFlowFiltersStrategy implements FlowSearchStrategy {
+  constructor(private readonly flowService: FlowService) {}
+
+  async search(args: FlowSearchArgs): Promise<FlowSearchStrategyResponse> {
+    const {
+      databaseConnection,
+      models,
+      flowFilters,
+      orderBy,
+      limit,
+      offset,
+      statusFilter,
+    } = args;
+    // Map flowConditions to where clause
+    let flowConditions = prepareFlowConditions(flowFilters);
+
+    // Add status filter conditions if provided
+    flowConditions = prepareFlowStatusConditions(flowConditions, statusFilter);
+
+    // Build conditions object
+    // We need to add the condition to filter the deletedAt field
+    const whereClause: FlowWhere = {
+      [Cond.AND]: [
+        {
+          deletedAt: {
+            [Op.IS_NULL]: true,
+          },
+        },
+        flowConditions ?? {},
+      ],
+    };
+
+    const orderByFlow = mapFlowOrderBy(orderBy);
+
+    const [flows, countRes] = await Promise.all([
+      this.flowService.getFlows({
+        models,
+        conditions: whereClause,
+        offset,
+        orderBy: orderByFlow,
+        limit,
+      }),
+      this.flowService.getFlowsCount(databaseConnection, whereClause),
+    ]);
+
+    // Map count result query to count object
+    const countObject = countRes;
+
+    return { flows, count: countObject };
+  }
+}
---
+++ src/domain-services/flows/strategy/impl/utils.ts
@@ -0,0 +1,388 @@
+import { type FlowId } from '@unocha/hpc-api-core/src/db/models/flow';
+import { Cond, Op } from '@unocha/hpc-api-core/src/db/util/conditions';
+import { createBrandedValue } from '@unocha/hpc-api-core/src/util/types';
+import type Knex from 'knex';
+import { type OrderBy } from '../../../../utils/database-types';
+import { type EntityDirection } from '../../../base-types';
+import type {
+  FlowObjectFilterGrouped,
+  FlowObjectType,
+} from '../../../flow-object/model';
+import type {
+  FlowCategory,
+  FlowObjectFilters,
+  SearchFlowsFilters,
+} from '../../graphql/args';
+import { type FlowStatusFilter } from '../../graphql/types';
+import type {
+  FlowInstance,
+  FlowOrderBy,
+  FlowOrderByCond,
+  FlowWhere,
+  UniqueFlowEntity,
+} from '../../model';
+
+export const mapFlowCategoryConditionsToWhereClause = (
+  flowCategoryConditions: FlowCategory[]
+) => {
+  if (flowCategoryConditions.length > 0) {
+    let whereClause = {};
+    // Map category filters
+    // getting Id when possible
+    // or name and group otherwise
+    const categoryIdFilters: number[] = [];
+    const categoryFilters = new Map<string, string[]>();
+    for (const categoryFilter of flowCategoryConditions) {
+      if (categoryFilter.id) {
+        categoryIdFilters.push(categoryFilter.id);
+      } else if (categoryFilter.group && categoryFilter.name) {
+        const group = categoryFilter.group;
+        const name = categoryFilter.name;
+
+        const groupsNamesFilter = categoryFilters.get(group) ?? [];
+
+        groupsNamesFilter.push(name);
+        categoryFilters.set(group, groupsNamesFilter);
+      }
+    }
+
+    if (categoryIdFilters.length > 0) {
+      whereClause = {
+        ...whereClause,
+        id: {
+          [Op.IN]: categoryIdFilters,
+        },
+      };
+    }
+
+    // For each entry of the group name
+    // add a condition to the where clause
+    // with the names associated to the group
+    // both in the same AND clause
+    for (const [group, names] of categoryFilters) {
+      whereClause = {
+        ...whereClause,
+        [Cond.AND]: [
+          {
+            group: {
+              [Op.LIKE]: group,
+            },
+            name: {
+              [Op.IN]: names,
+            },
+          },
+        ],
+      };
+    }
+    return whereClause;
+  }
+
+  return null;
+};
+
+export const mergeFlowIDsFromFilteredFlowObjectsAndFlowCategories = (
+  flowIDsFromFilteredFlowObjects: FlowId[],
+  flowIDsFromFilteredFlowCategories: FlowId[]
+): FlowId[] => {
+  const isFlowIDsFromFilteredFlowCategoriesIsEmpty =
+    !flowIDsFromFilteredFlowCategories?.length;
+  const isFlowIDsFromFilteredFlowObjectsIsEmpty =
+    !flowIDsFromFilteredFlowObjects?.length;
+
+  if (
+    isFlowIDsFromFilteredFlowCategoriesIsEmpty &&
+    isFlowIDsFromFilteredFlowObjectsIsEmpty
+  ) {
+    return [];
+  }
+
+  if (
+    isFlowIDsFromFilteredFlowCategoriesIsEmpty &&
+    !isFlowIDsFromFilteredFlowObjectsIsEmpty
+  ) {
+    return flowIDsFromFilteredFlowObjects;
+  }
+
+  if (
+    !isFlowIDsFromFilteredFlowCategoriesIsEmpty &&
+    isFlowIDsFromFilteredFlowObjectsIsEmpty
+  ) {
+    return flowIDsFromFilteredFlowCategories;
+  }
+
+  return flowIDsFromFilteredFlowObjects.length >
+    flowIDsFromFilteredFlowCategories.length
+    ? flowIDsFromFilteredFlowCategories.filter((flowID) =>
+        flowIDsFromFilteredFlowObjects.includes(flowID)
+      )
+    : flowIDsFromFilteredFlowObjects.filter((flowID) =>
+        flowIDsFromFilteredFlowCategories.includes(flowID)
+      );
+};
+
+export const sortingColumnMapping: Map<string, string> = new Map<
+  string,
+  string
+>([
+  ['reporterRefCode', 'refCode'],
+  ['sourceID', 'sourceID'],
+]);
+
+export const mapFlowOrderBy = (orderBy?: FlowOrderBy): FlowOrderByCond => {
+  if (!orderBy || orderBy.entity !== 'flow') {
+    return defaultFlowOrderBy();
+  }
+
+  return { column: orderBy.column as keyof FlowInstance, order: orderBy.order };
+};
+
+export const mapOrderByToEntityOrderBy = (orderBy?: FlowOrderBy): OrderBy => {
+  if (!orderBy || orderBy.entity === 'flow') {
+    return defaultFlowOrderBy();
+  }
+
+  let columnToSort: string;
+  if (sortingColumnMapping.has(orderBy.column)) {
+    // I don't like this but the compiler is complaining
+    // that columnToSort might be undefined if I don't do this
+    // but it's already checked that the column exists in the map
+    columnToSort = sortingColumnMapping.get(orderBy.column) ?? 'updatedAt';
+  } else {
+    columnToSort = orderBy.column;
+  }
+
+  return { column: columnToSort, order: orderBy.order };
+};
+
+export const defaultFlowOrderBy = (): FlowOrderByCond => {
+  return { column: 'updatedAt', order: 'desc' };
+};
+
+export const buildOrderByReference = (
+  refList: UniqueFlowEntity[]
+): FlowOrderByCond => {
+  if (refList.length === 0) {
+    return defaultFlowOrderBy();
+  }
+
+  return {
+    column: 'id',
+    raw: `array_position(ARRAY[${refList
+      .map((entry) => entry.id)
+      .join(',')}], "id")`,
+  };
+};
+
+export const prepareFlowConditions = (
+  flowFilters: SearchFlowsFilters
+): FlowWhere => {
+  let flowConditions = {};
+
+  if (flowFilters) {
+    for (const [key, value] of Object.entries(flowFilters)) {
+      if (value !== undefined) {
+        flowConditions = { ...flowConditions, [key]: value };
+      }
+    }
+  }
+
+  return flowConditions satisfies FlowWhere;
+};
+
+export const mergeUniqueEntities = (
+  listA: UniqueFlowEntity[],
+  listB: UniqueFlowEntity[]
+): UniqueFlowEntity[] => {
+  if (listA.length === 0) {
+    return listB;
+  }
+
+  if (listB.length === 0) {
+    return listA;
+  }
+
+  // Convert the lists into a set for efficient lookup
+  const entityMapListA = new Set(listA.map(mapUniqueFlowEntitisSetKeyToSetkey));
+
+  const entityMapListB = new Set(listB.map(mapUniqueFlowEntitisSetKeyToSetkey));
+
+  for (const key of entityMapListB) {
+    if (!entityMapListA.has(key)) {
+      entityMapListA.add(key);
+    }
+  }
+
+  // Convert the keys back to UniqueFlowEntity objects
+  return mapUniqueFlowEntitisSetKeyToUniqueFlowEntity(entityMapListA);
+};
+
+export const intersectUniqueFlowEntities = (
+  ...lists: UniqueFlowEntity[][]
+): UniqueFlowEntity[] => {
+  // If any of the lists is empty, remove it
+  lists = lists.filter((list) => list.length > 0);
+
+  if (lists.length === 0) {
+    return [];
+  }
+
+  if (lists.length === 1) {
+    return lists[0];
+  }
+
+  // Convert the first list into a set for efficient lookup
+  const initialSet = new Set(lists[0].map(mapUniqueFlowEntitisSetKeyToSetkey));
+
+  // Intersect the remaining lists with the initial set
+  for (let i = 1; i < lists.length; i++) {
+    const currentSet = new Set(
+      lists[i].map(mapUniqueFlowEntitisSetKeyToSetkey)
+    );
+    for (const key of initialSet) {
+      if (!currentSet.has(key)) {
+        initialSet.delete(key);
+      }
+    }
+  }
+
+  // Convert the keys back to UniqueFlowEntity objects
+  return mapUniqueFlowEntitisSetKeyToUniqueFlowEntity(initialSet);
+};
+
+export const mapUniqueFlowEntitisSetKeyToSetkey = (
+  entity: UniqueFlowEntity
+): string => {
+  return `${entity.id}_${entity.versionID}`;
+};
+
+export const mapUniqueFlowEntitisSetKeyToUniqueFlowEntity = (
+  set: Set<string>
+): UniqueFlowEntity[] => {
+  return [...set].map((key) => {
+    const [id, versionID] = key.split('_').map(Number);
+    return { id: createBrandedValue(id), versionID } satisfies UniqueFlowEntity;
+  });
+};
+
+export const removeDuplicatesUniqueFlowEntities = (
+  entities: UniqueFlowEntity[]
+): UniqueFlowEntity[] => {
+  const uniqueEntities = new Map<string, UniqueFlowEntity>();
+
+  for (const entity of entities) {
+    const key = `${entity.id}_${entity.versionID}`;
+    if (!uniqueEntities.has(key)) {
+      uniqueEntities.set(key, entity);
+    }
+  }
+
+  return [...uniqueEntities.values()];
+};
+
+// FIXME: to be removed once the new search is implemented
+export function applySearchFilters(
+  query: Knex.QueryBuilder,
+  filters: any
+): Knex.QueryBuilder {
+  // Check if 'id' filter is defined and apply it
+  if (filters.id !== null && filters.id !== undefined) {
+    query.whereIn('id', filters.id);
+  }
+
+  // Check if 'activeStatus' filter is defined and apply it
+  if (filters.activeStatus !== null && filters.activeStatus !== undefined) {
+    query.andWhere('activeStatus', filters.activeStatus);
+  }
+
+  // Check if 'amountUSD' filter is defined and apply it
+  if (filters.amountUSD !== null && filters.amountUSD !== undefined) {
+    query.andWhere('amountUSD', filters.amountUSD);
+  }
+
+  // Check if 'restricted' filter is defined and apply it
+  if (filters.restricted !== null && filters.restricted !== undefined) {
+    query.andWhere('restricted', filters.restricted);
+  }
+
+  // Check if 'versionID' filter is defined and apply it
+  if (filters.versionID !== null && filters.versionID !== undefined) {
+    query.andWhere('versionID', filters.versionID, 1);
+  }
+
+  return query;
+}
+
+// FIXME: to be removed once the new search is implemented
+export const prepareFlowStatusConditions = (
+  flowConditions: any,
+  statusFilter: FlowStatusFilter | null
+) => {
+  if (statusFilter) {
+    if (statusFilter === 'new') {
+      // Flows with version 1 are considered new
+      flowConditions = { ...flowConditions, versionID: '=' };
+    } else if (statusFilter === 'updated') {
+      // Flows with version greater than 1 are considered updated
+      flowConditions = { ...flowConditions, versionID: '>' };
+    }
+  }
+  return flowConditions;
+};
+
+export const buildSearchFlowsConditions = (
+  uniqueFlowEntities: UniqueFlowEntity[],
+  flowFilters?: SearchFlowsFilters
+): FlowWhere => {
+  const whereClauses = uniqueFlowEntities.map((flow) => ({
+    [Cond.AND]: [{ id: flow.id }, { versionID: flow.versionID }],
+  }));
+
+  if (flowFilters) {
+    const flowConditions = prepareFlowConditions(flowFilters);
+    return {
+      [Cond.AND]: [
+        { deletedAt: null },
+        flowConditions,
+        { [Cond.OR]: whereClauses },
+      ],
+    } satisfies FlowWhere;
+  }
+
+  return {
+    [Cond.OR]: whereClauses,
+  };
+};
+
+export const mapFlowFiltersToFlowObjectFiltersGrouped = (
+  flowObjectFilters: FlowObjectFilters[]
+): FlowObjectFilterGrouped => {
+  const flowObjectFilterGrouped = new Map<
+    FlowObjectType,
+    Map<EntityDirection, number[]>
+  >();
+
+  for (const flowObjectFilter of flowObjectFilters) {
+    const objectType = flowObjectFilter.objectType;
+    const flowDirection = flowObjectFilter.direction;
+    const objectId = flowObjectFilter.objectID;
+
+    // Get the map of flow object IDs for the given object type
+    // Or create a new map if it doesn't exist
+    const directionWithIDsMap =
+      flowObjectFilterGrouped.get(objectType) ??
+      new Map<EntityDirection, number[]>();
+
+    // Get the list of flow object IDs for the given direction
+    // Or create a new list if it doesn't exist
+    const flowObjectIDs = directionWithIDsMap.get(flowDirection) ?? [];
+    flowObjectIDs.push(objectId);
+
+    // Update the map with the new list of flow object IDs for the given direction
+    directionWithIDsMap.set(flowDirection, flowObjectIDs);
+
+    // Update the map with the new map of direction+ids for the given object type
+    flowObjectFilterGrouped.set(objectType, directionWithIDsMap);
+  }
+
+  return flowObjectFilterGrouped;
+};
