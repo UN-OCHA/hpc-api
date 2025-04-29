f1f899e585f9d92a859756110c41cf5d290c7c31
Pl217 <pl_217@live.com>
2025-04-29 22:47:05 +0200

    Set up formatting for Husky files
    Because files in `.husky` directory don't have
    an extension, Prettier cannot treat them as shell
    scripts, so we need to configure it.
    
    But, we only want formatting of user-added files
    and not in Husky's own `.husky/_/` sub-directory.

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,1 +1,14 @@
-export { default } from '@unocha/hpc-repo-tools/prettier.config.base.js';
+import prettierConfigBase from '@unocha/hpc-repo-tools/prettier.config.base.js';
+
+export default {
+  ...prettierConfigBase,
+  overrides: [
+    ...prettierConfigBase.overrides,
+    {
+      files: ['.husky/*'],
+      options: {
+        parser: 'sh',
+      },
+    },
+  ],
+};
