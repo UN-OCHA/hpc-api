9ca18cbb94b92a46c55a377197d347431cb186de
manelcecs <manuelc.p.g@gmail.com>
2025-05-14 09:15:20 +0200

    🏃 Increased performance when obtaining 'Parked Parents Flows'

--- src/domain-services/flow-link/flow-link-service.ts
+++ src/domain-services/flow-link/flow-link-service.ts
@@ -1,6 +1,6 @@
 import { type FlowId } from '@unocha/hpc-api-core/src/db/models/flow';
 import { type Database } from '@unocha/hpc-api-core/src/db/type';
-import { Op } from '@unocha/hpc-api-core/src/db/util/conditions';
+import { Cond, Op } from '@unocha/hpc-api-core/src/db/util/conditions';
 import { type InstanceOfModel } from '@unocha/hpc-api-core/src/db/util/types';
 import { getOrCreate } from '@unocha/hpc-api-core/src/util';
 import { Service } from 'typedi';
@@ -14,26 +14,40 @@ export class FlowLinkService {
     // Fetch all flow links in one go
     const flowLinks = await models.flowLink.find({
       where: {
-        parentID: {
-          [Op.IN]: flowIds,
-        },
+        [Cond.AND]: [
+          {
+            [Cond.OR]: [
+              {
+                parentID: {
+                  [Op.IN]: flowIds,
+                },
+              },
+              {
+                childID: {
+                  [Op.IN]: flowIds,
+                },
+              },
+            ],
+          },
+          {
+            depth: { [Op.GTE]: 1 },
+          },
+        ],
       },
     });
 
     // Initialize the map with empty arrays for each flowId
     const flowLinksMap = new Map<
-      number,
+      FlowId,
       Array<InstanceOfModel<Database['flowLink']>>
     >();
 
-    // Group flow links by parentID in one pass
+    // Group flow links by `parentID` and `childID` in one pass
     for (const link of flowLinks) {
-      const flowLinksForFlow = getOrCreate(
-        flowLinksMap,
-        link.parentID,
-        () => []
-      );
-      flowLinksForFlow.push(link);
+      const childFlowLink = getOrCreate(flowLinksMap, link.childID, () => []);
+      const parentFlowLink = getOrCreate(flowLinksMap, link.parentID, () => []);
+      childFlowLink.push(link);
+      parentFlowLink.push(link);
     }
 
     return flowLinksMap;
